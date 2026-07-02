ee6e6c30d248c807314d5717505a24f3ad4eabcf
Rather Aadil <ratheraadil61@gmail.com>
2026-07-02 23:13:23 +0530

    Learn about types as sets of js values

---
+++ S06_Demystifing_Unions_and_Intersections/05_Types_as_Sets_of_Javascript_values/app.ts
@@ -0,0 +1,10 @@
+// In Typescript, a type is the set of possible javascript values.
+const unviersal: unknown = 'Any kind of value'; // This can be considered as Universal set
+
+const num: number = 12; // set of numbers
+const str: string = 'Hi'; // set of strings
+const bool: boolean = true;
+const undef: undefined = undefined; // singleton set
+const obj: object = []; // Set of objects
+
+let emptySet: never; // empty set
